2d35951c1a6998b0b2a6dc953b985efc99093626
Anton <164165753+AkoHb@users.noreply.github.com>
2024-11-10 00:46:48 +0200

    Create InitialValues.js

---
+++ src/Components/Data/InitialValues.js
@@ -0,0 +1,10 @@
+export const InitialValues = {
+    break: 5,
+    session: 25,
+    timer: 25 * 60,
+    timerIsWorking: false,
+    currentTimeInterval: "session",
+    decr: "<",
+    incr: ">"
+
+}
